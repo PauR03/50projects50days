b90225d4e1981710a11f3ab8a62622fa31c3bab7
Pau Rius <priusnori03@gmail.com>
2023-08-30 15:29:33 +0200

    .

---
+++ progressSteps/script.js
@@ -0,0 +1,33 @@
+window.addEventListener("load", () => {
+    const progressLine = document.getElementById("line")
+    const steps = document.querySelectorAll(".step")
+
+    const prevButton = document.getElementById("prev")
+    const nextButton = document.getElementById("next")
+
+    let progress = 0
+
+    prevButton.addEventListener("click", () => {
+        steps[progress].classList.remove("completed")
+        progress--
+        progressLine.style.setProperty("width", 100 / (steps.length - 1) * progress +"%")
+
+        if (progress === 0) prevButton.setAttribute("disabled", "true")
+        if(progress > 0)nextButton.removeAttribute('disabled')
+        
+    })
+
+    nextButton.addEventListener("click", () => {
+        progress++
+        steps[progress].classList.add("completed")
+        progressLine.style.setProperty("width", 100 / (steps.length - 1) * progress +"%")
+
+        console.log(100 / (steps.length - 1) * progress)
+
+        if(progress === steps.length - 1) nextButton.setAttribute("disabled", "true")
+        if(progress > 0)prevButton.removeAttribute('disabled')
+    })
+})
+
+
+
